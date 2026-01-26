54d7394373fcb1f9bb72fe01f4dd3eacbd04269c
florykhan <florykhan@gmail.com>
2026-01-26 08:54:04 -0800

    Redeploy site to GitHub Pages from main branch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import "./dashboard.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "leaflet/dist/leaflet.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.jsx";
 import CoverageMapPage from "./pages/CoverageMapPage.jsx";
 import { fixLeafletIcons } from "./lib/leafletIcons";
@@ -12,12 +12,12 @@ fixLeafletIcons();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/map" element={<CoverageMapPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
